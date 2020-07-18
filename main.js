b4f62978fff12217a18d627fc73b202ad8b582c4
Jimmy <slimjim49j@gmail.com>
2020-07-17 17:50:11 -0700

    Add projects show screen

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ import anime from './anime-master/lib/anime.es.js';
 //     duration: 5000,
 // });
 
-anime({
+const tl = anime.timeline({ easing: "easeInOutCirc" });
+tl
+.add({
     targets: "#main-circle",
     scale: [
         {
@@ -24,12 +26,20 @@ anime({
         }
     ],
 })
+.add({
+    targets: ".projects-section",
+    // right: "-60%",
+    opacity: 1,
+    duration: 500,
+})
 
 
 window.setTimeout(() => {
     const mainCircle = document.querySelector("#main-circle");
+    const projectsSection = document.querySelector(".projects-section")
     
-    mainCircle.addEventListener("mouseenter", () => {
+    mainCircle.addEventListener("mouseenter", (e) => {
+        if (document.querySelector("#main-circle-container").style.visibility === "hidden") return;
         if (Array.from(mainCircle.classList).includes("flipped")) return;
         anime.remove('#main-circle');
     
@@ -47,17 +57,14 @@ window.setTimeout(() => {
             borderColor: "#47b290",
             duration: 1000,
         }, "-=100")
-        .add({
-            targets: ".projects-section",
-            right: "-60%",
-            opacity: 1,
-            duration: 500,
-        }, "-=700")
+        
         console.log("heya")
     })
 
-    mainCircle.addEventListener("mouseleave", () => {
+    mainCircle.addEventListener("mouseleave", (e) => {
+        if (Array.from(e.path).includes(projectsSection)) return;
         if (Array.from(mainCircle.classList).includes("flipped")) return;
+        if (document.querySelector("#main-circle-container").style.visibility === "hidden") return;
         anime.remove('#main-circle');
 
         const tl = anime.timeline({ easing: "easeInOutCirc"});
@@ -74,19 +81,52 @@ window.setTimeout(() => {
             borderColor: "#fff",
             duration: 1000,
         }, "-=100")
-        .add({
-            targets: ".projects-section",
-            opacity: 0,
-            // right: "40%",
-            duration: 500,
-        }, "-=700")
+        // .add({
+        //     targets: ".projects-section",
+        //     opacity: 0,
+        //     // right: "40%",
+        //     duration: 500,
+        // }, "-=700")
     })
 
     document.querySelector(".main-text-container").addEventListener("click", () => {
         mainCircle.classList.toggle("flipped");
     })
 
-    document.querySelector(".projects-section");
+    projectsSection.addEventListener("click", () => {
+        document.getElementById("main-circle-container").style.visibility = "hidden";
+        projectsSection.classList.remove("minified");
+        // projectsSection.querySelector("").style.flexDirection = "row";
+
+        // const tl = anime.timeline({ easing: "easeInOutCirc" });
+        // tl
+        // .add({
+        //     targets: ".projects-section",
+        //     easing: "easeInOutCirc",
+        //     width: "90vw",
+        //     height: "90vh",
+        //     right: "-100%",
+        //     duration: 400,
+        // })
+        // .add({
+        //     targets: ".projects-section ul",
+        //     height: "90%",
+        //     duration: 200,
+        // })
+        // .add({
+        //     targets: ".projects-section li",
+        //     height: "100%",
+        //     border: "1px solid red",
+        //     duration: 200,
+        // })
+
+    })
+
+    document.querySelector(".projects-exit-btn").addEventListener("click", (e) => {
+        e.stopPropagation();
+        document.getElementById("main-circle-container").style.visibility = "visible";
+        projectsSection.classList.add("minified");
+    })
 }, 600)
 
 
